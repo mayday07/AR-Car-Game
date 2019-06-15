e715f2b3e35b00e2bca70f3efbc4caff9a870e0f
manildayma <manil.dayma.98@gmail.com>
2019-06-16 02:50:40 +0530

    updated

---
+++ Assets/Data/Script/moveCar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class moveCar : MonoBehaviour
+{
+    //speed    
+   public float speed = 10.0f;
+   //left right
+   public float rotationspeed = 100.0f;
+   void Update()
+   {
+       //joystick
+       float translation = CrossPlatformInputManager.GetAxis("Vertical") * speed;
+       float rotation = CrossPlatformInputManager.GetAxis("Horizontal") * rotationspeed;
+       translation *= Time.deltaTime;
+       rotation *= Time.deltaTime;
+       transform.Translate(-translation, 0, 0);
+       transform.Rotate(0, rotation, 0);       
+    }
+}
---
+++ Assets/Data/Script/resetScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class resetScene : MonoBehaviour
+{
+    public void resartGame(){
+        SceneManager.LoadScene("raceTrack");
+    }
+}
