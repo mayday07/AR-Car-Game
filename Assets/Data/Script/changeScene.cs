f5d88bcb43c607818edff3f84921dee87377eeee
manildayma <manil.dayma.98@gmail.com>
2019-06-27 00:55:23 +0530

    updated (UI, tracks, material)

---
+++ Assets/Data/Script/changeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+    public void FirstToMain(){ SceneManager.LoadScene("main"); }
+    public void MainToStart(){ SceneManager.LoadScene("raceTrack1"); }
+    public void MainToMode(){ SceneManager.LoadScene("mode"); }
+    public void MainToHow(){ SceneManager.LoadScene("how"); }
+    public void HowToMain(){ SceneManager.LoadScene("main"); }
+    public void ModeToMain(){ SceneManager.LoadScene("main"); }
+    public void RaceToMain(){ SceneManager.LoadScene("main"); }
+    public void ResartGame(){ SceneManager.LoadScene("raceTrack1"); }
+
+
+
+
+    
+}
